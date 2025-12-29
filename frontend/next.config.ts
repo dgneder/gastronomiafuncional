75d2f6175d7a09edbd0e03e8f893d3a31f8594d8
dgneder <dgneder@gmail.com>
2025-12-29 02:10:27 -0300

    chore: tirando standalone

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  
   images: {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 dias de cache
   },
-
   // Otimizações adicionais
   compress: true,
   poweredByHeader: false,
